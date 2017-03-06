d3e317c29c62b13a6ff1a787c53c2334492d6738
Robin Thrift <services@robinthrift.com>
2017-03-06 09:34:10 +0100

    feat(cli): start server

--- main.go
+++ main.go
@@ -1,4 +1,21 @@
 package main
 
+import "os"
+
+func getenv(key, fallback string) string {
+	value := os.Getenv(key)
+	if len(value) == 0 {
+		return fallback
+	}
+	return value
+}
+
 func main() {
+	port := getenv("TIL_PORT", "3000")
+	secret := getenv("TIL_SECRET", "PLEASE_SET_A_SECRET")
+	basePath := getenv("TIL_BASE_PATH", "_tils")
+
+	os.MkdirAll(basePath, os.FileMode(0755))
+
+	startServer(port, secret, basePath)
 }
